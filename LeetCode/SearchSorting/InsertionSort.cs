8c557c2a27edf1b1baaa8d5168314a56aea4ad1b
princa <princa@gmail.com>
2014-12-03 00:19:10 -0500

    Added Insertion sort & shell sort

---
+++ LeetCode/SearchSorting/InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.SearchSorting
+{
+    //Insertion sort - take first element, compare all its previous elements, and if previous is greater than element
+    //all element move forward 1 position, and move the smallest one to the far left
+    public class InsertionSort
+    {
+        public static int[] DoInsertionSort(int[] array)
+        {
+            int i, k, j;
+            for (i = 0; i < array.Length; i++)
+            {
+                k = array[i];
+                j = i - 1;
+                while (j > 0 && array[j] > k)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j+1] = k;
+            }
+            return array;
+        }
+    }
+}
